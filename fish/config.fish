ff52a71b825461ef3c0b771f712a4668ffde73be
mamiethyst <mamiethyst@ebay.com>
2022-10-25 10:41:17 -0700

    Added path.  Added press_any_key script

--- fish/config.fish
+++ fish/config.fish
@@ -22,6 +22,7 @@ fish_add_path ~/bin
 # other defines
 set -Ux FISH_CONFIG_PATH ~/.config/fish/config.fish
 set -Ux OSS_HOME ~/dev/oss
+fish_add_path $OSS_HOME/Bashpile/bin
 
 ## company specific
 set COMPANY ~/.config/fish/company.fish
